3a1ceba3a17e9325397a3bc7539ac2edd5449f9f
boonleng <boonleng@ou.edu>
2017-09-19 12:54:54 -0500

    Separated data retrieving

---
+++ data.py
@@ -0,0 +1,49 @@
+import datetime
+import pandas_datareader
+import requests_cache
+
+# Some global variables
+N = 1350;
+
+# Fang - GOOG, NFLX, AMZN, FB
+# Chip - MU, AMAT, MRVL, NVDA
+    # '^DJI', '^GSPC', '^IXIC',
+indices = ['^DJI', '^GSPC', '^IXIC']
+symbols = [
+    'AAPL', 'TSLA',
+    'GOOG', 'BIDU', 'MSFT',
+    'NVDA', 'AMAT', 'MRVL', 'MU', 'AMD', 'INTC',
+    'AMZN', 'NFLX', 'EBAY', 'BABA', 'BKS',
+    'FB', 'TWTR', 'YELP',
+    'NDLS', 'CMG', 'MCD',
+    'S', 'T', 'VZ', 'TMUS', 'QCOM',
+    'SBUX',
+    'STX', 'WDC', 'DVMT', 'TXN', 'ADI', 'MCHP',
+    'SNE',
+    'C', 'V', 'BAC', 'WFC', 'AMTD',
+    'BP', 'XON', 'CVX', 'OGE', 'JASO',
+    'F', 'GM', 'TM'
+]
+
+# Specify end day, then roll back the maximum SMA window, plus another week
+end = datetime.date(2017, 9, 15)
+start = end - datetime.timedelta(days = N)
+
+def get_old_data():
+    print('Loading data since ' + str(start) + ' ...')
+    session = requests_cache.CachedSession(cache_name = 'cache-big', backend = 'sqlite', expire_after = datetime.timedelta(days = 60))
+    quotes = pandas_datareader.DataReader(symbols, 'google', start, end, session = session)
+    return quotes
+
+def get_old_indices():
+    session = requests_cache.CachedSession(cache_name = 'cache-idx', backend = 'sqlite', expire_after = datetime.timedelta(days = 60))
+    quotes = pandas_datareader.DataReader(indices, 'yahoo', start, end, session = session)
+    return quotes
+
+def get():
+    end = datetime.date.today()
+    start = end - datetime.timedelta(days = N)
+    print('Loading data since from ' + str(start) + ' to ' + str(end) + ' ...')
+    session = requests_cache.CachedSession(cache_name = 'cache-sub', backend = 'sqlite', expire_after = datetime.timedelta(days = 1))
+    quotes = pandas_datareader.DataReader(symbols, 'google', start, end, session = session)
+    return quotes
---
+++ speed.py
@@ -0,0 +1,58 @@
+import datetime
+import numpy as np
+import matplotlib
+import matplotlib.pyplot
+import chart
+import data
+import time
+# import tensorflow as tf
+
+# Some global variables
+K = 100                       # Number of days to show
+sma_sizes = [10, 50, 100]     # SMA window sizes
+
+# Some default plotting attributes
+matplotlib.rcParams['font.family'] = 'serif'
+matplotlib.rcParams['font.serif'] = ['Arial']
+matplotlib.rcParams['font.sans-serif'] = ['System Font', 'Verdana', 'Arial']
+matplotlib.rcParams['figure.figsize'] = (8, 4)   # Change the size of plots
+matplotlib.rcParams['figure.dpi'] = 108
+
+quotes = data.get_old_data()
+
+# Method 0 or 1
+for method in [0, 1]:
+
+    print('Method {}:'.format(method))
+
+    # Set up the chart
+    if method is 1:
+        print('Preparing figure ...')
+        view = chart.Chart(K)
+        view.set_xdata(quotes.major_axis[-K:])
+        #matplotlib.pyplot.show(block = False)
+
+    # Make sure we get enough data so that all SMA curves are valid
+    K = K + max(sma_sizes)
+    sss = quotes[:, quotes.axes[1][-K:], :]
+    print(sss)
+
+    # Go through the symbols
+    t1 = time.time()
+    for symbol in quotes.minor_axis:
+        if method is 0:
+            view = chart.showChart(sss[:, :, [symbol]], color_scheme = 'sunrise')
+        else:
+            view.set_data(sss[:, :, [symbol]])
+            #view.fig.canvas.draw()
+            #view.fig.canvas.flush_events()
+            #matplotlib.pyplot.pause(0.001)
+
+        print('-> \033[38;5;46m{}\033[0m'.format(symbol.rjust(4)))
+        # Close it if needed
+        if method is 0:
+            matplotlib.pyplot.close(view['figure'])
+
+    t0 = time.time()
+
+    print('Elapsed time: {0:.3f} s'.format(t0 - t1))
