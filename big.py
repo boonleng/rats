956a901d313b3ae21b0c837f9baa4fe50ed0696a
boonleng <boonleng@ou.edu>
2017-09-22 09:49:40 -0500

    Working on it ...

--- big.py
+++ big.py
@@ -1,27 +1,48 @@
 import data
-# import tensorflow as tf
+import matplotlib
+import tensorflow as tf
 
 quotes = data.get_old_data()
 # quotes = data.get_old_data(reload = True)
+sym = 'NVDA'
+data = quotes[:, :, sym]
+yy = data.loc[:, 'Close'].tolist()
+
+# print(quotes)
+
+print('Setting up \033[38;5;214mTensorflow\033[0m ...')
+
+W = tf.Variable([0.3], dtype=tf.float32)
+b = tf.Variable([-0.8], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32)
+y_ = W * x + b
+
+loss = tf.reduce_sum(tf.square(y_ - y))
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+# Training data
+# x_train = list(matplotlib.dates.date2num(data.index[0:-5].tolist()))
+x_train = list(range(len(yy) - 5))
+y_train = yy[0:-5]
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+print('Training ...')
+for i in range(1000):
+    sess.run(train, {x: x_train, y: y_train})
+
+W_c, b_c, loss_c = sess.run([W, b, loss], {x: x_train, y: y_train})
+print("W: %s b: %s loss: %s" % (W_c, b_c, loss_c))
 
 # Create the model
-L = quotes.shape[2]
-# x = tf.placeholder(tf.float32, [None, L])
-# w = tf.Variable(tf.zeross[L, L])
-# b = tf.Variable(tf.zeross[L, L])
+# L = quotes.shape[2]
+# x = tf.placeholder(tf.float32, [None, L], label = 'x')
+# w = tf.Variable(tf.zeros([L, L]), label = 'w')
+# b = tf.Variable(tf.zeros([L, L]), label = 'b')
 # y = tf.matmul(x, W) + b
 
 # y_ = tf.placeholder(tf.float32, [None, L])
-
-# cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(label = y_, logits = y))
-# train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-# sess = tf.InteractiveSession()
-# tf.global_variables_initializer().run()
-
-# for i in range(L):
-
-# print('Saving to offline files ...')
-# data.save_to_folder(quotes)
-
-print(quotes)
--- data.py
+++ data.py
@@ -25,6 +25,9 @@ symbols = [
 ]
 
 def get_old_data(folder = 'data', reload = False):
+    """
+        Get 5 years worth of data on the selected symbols
+    """
     if os.path.exists(folder) and not reload:
         import re
         import glob
@@ -38,7 +41,7 @@ def get_old_data(folder = 'data', reload = False):
                 local_symbols.append(symbol)
         # Read the last one for the data dimensions
         df = pandas.read_pickle(file)
-        print('Loading offline data since ' + str(df.index[0].strftime('%Y-%m-%d')) + ' to ' + str(df.index[-1].strftime('%Y-%m-%d')) + ' ...')
+        print('Loading offline data from ' + str(df.index[0].strftime('%Y-%m-%d')) + ' to ' + str(df.index[-1].strftime('%Y-%m-%d')) + ' ...')
         #dd = np.empty([df.shape[0], df.shape[1], len(symbols)])
         dd = np.empty([df.shape[1], df.shape[0], len(local_symbols)])
         # Now we go through the files again and read them this time
@@ -50,7 +53,7 @@ def get_old_data(folder = 'data', reload = False):
     else:
         end = datetime.date(2017, 9, 21)
         start = end - datetime.timedelta(days = 5 * 365)
-        print('Loading data since ' + str(start) + ' to ' + str(end) + ' ...')
+        print('Loading data from ' + str(start) + ' to ' + str(end) + ' ...')
         session = requests_cache.CachedSession(cache_name = 'cache-big', backend = 'sqlite', expire_after = datetime.timedelta(days = 60))
         quotes = pandas_datareader.DataReader(symbols, 'yahoo', start, end, session = session)
     return quotes
@@ -63,12 +66,12 @@ def get_old_indices():
     quotes = pandas_datareader.DataReader(indices, 'yahoo', start, end, session = session)
     return quotes
 
-def get():
+def get(engine = 'yahoo'):
     end = datetime.date.today()
     start = end - datetime.timedelta(days = 250)
-    print('Loading data since from ' + str(start) + ' to ' + str(end) + ' ...')
+    print('Loading data from ' + str(start) + ' to ' + str(end) + ' ...')
     session = requests_cache.CachedSession(cache_name = 'cache-sub', backend = 'sqlite', expire_after = datetime.timedelta(days = 1))
-    quotes = pandas_datareader.DataReader(symbols, 'google', start, end, session = session)
+    quotes = pandas_datareader.DataReader(symbols, engine, start, end, session = session)
     return quotes
 
 def save_to_folder(quotes, folder = 'data'):
