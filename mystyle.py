58f7c9dfad6cd486e51df22b5e675acfba59db06
boonleng <boonleng@ou.edu>
2017-09-24 21:45:28 -0500

    Added mystyle.py

---
+++ mystyle.py
@@ -0,0 +1,8 @@
+import matplotlib
+
+# Some default plotting attributes
+matplotlib.rcParams['font.family'] = 'serif'
+matplotlib.rcParams['font.serif'] = ['Arial']
+matplotlib.rcParams['font.sans-serif'] = ['System Font', 'Verdana', 'Arial']
+matplotlib.rcParams['figure.figsize'] = (7, 4)   # Change the size of plots
+matplotlib.rcParams['figure.dpi'] = 108
