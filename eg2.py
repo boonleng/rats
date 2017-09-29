f0f3f591322edd4521b538d1237aeac4c0daf587
boonleng <boonleng@ou.edu>
2017-09-29 12:42:41 -0500

    NN seems to compile alright

--- eg2.py
+++ eg2.py
@@ -21,32 +21,32 @@ def nn(x):
         Neural network for analyzing the time series
     """
     with tf.name_scope('reshape'):
-        x_rect = tf.reshape(x, [-1, 26, 1])
+        x_rect = tf.reshape(x, [-1, 26, 1, 1])
 
     with tf.name_scope('conv1'):
-        W_conv1 = weight_variable([5, 1, 26])
-        b_conv1 = bias_variable([26])
-        h_conv1 = tf.nn.relu(tf.nn.conv1d(x_rect, W_conv1, stride = 1, padding = 'SAME') + b_conv1)
+        W_conv1 = weight_variable([5, 1, 1, 16])
+        b_conv1 = bias_variable([16])
+        h_conv1 = tf.nn.relu(tf.nn.conv2d(x_rect, W_conv1, strides = [1, 1, 1, 1], padding = 'SAME') + b_conv1)
 
     with tf.name_scope('pool1'):
-        h_pool1 = tf.nn.max_pool(h_conv1, ksize = [1, 1, 2, 1], strides = [1, 1, 2, 1], padding = 'SAME')
+        h_pool1 = tf.nn.max_pool(h_conv1, ksize = [1, 2, 1, 1], strides = [1, 2, 1, 1], padding = 'SAME')
 
     with tf.name_scope('fc'):
-        W_fc = weight_variable([13 * 16, 1024])
-        b_fc = bias_variable([1024])
+        W_fc = weight_variable([13 * 16, 512])
+        b_fc = bias_variable([512])
 
-        h_pool2_flat = tf.reshape(h_pool1, [-1, 13 * 16])
-        h_fc = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc) + b_fc)
+        h_pool1_flat = tf.reshape(h_pool1, [-1, 13 * 16])
+        h_fc = tf.nn.relu(tf.matmul(h_pool1_flat, W_fc) + b_fc)
 
     with tf.name_scope('droptout'):
         keep_prob = tf.placeholder(tf.float32)
-        h_fc_drop = tf.nn.droptout(h_fc, keep_prob)
+        h_fc_drop = tf.nn.dropout(h_fc, keep_prob)
 
     with tf.name_scope('fc2'):
-        W_fc2 = weight_variable([1024, 2])
+        W_fc2 = weight_variable([512, 2])
         b_fc2 = bias_variable([2])
 
-        y_conv = tf.matmul(h_fc_drop, W_fc) + b_fc2
+        y_conv = tf.matmul(h_fc_drop, W_fc2) + b_fc2
     return y_conv, keep_prob
 
 def weight_variable(shape):
@@ -66,12 +66,20 @@ x = tf.placeholder(tf.float32, [None, 26])
 y_ = tf.placeholder(tf.float32, [None, 10])
 y, keep_prob = nn(x)
 
-with tf.name_scope('loss'):
-    loss = tf.nn.reduce_mean(tf.square(y_conv - y_))
+#with tf.name_scope('loss'):
+#	cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels = y_, logits = y)
+#    loss = tf.reduce_mean(tf.square(y_conv - y_))
+#
+#train = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
-train = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
-
-graph_location = './'
+graph_location = './eg2'
 print('Saving graph to: %s' % graph_location)
 train_writer = tf.summary.FileWriter(graph_location)
 train_writer.add_graph(tf.get_default_graph())
+
+with tf.Session() as sess:
+	tf.global_variables_initializer().run()
+
+#sess = tf.Session()
+#ss.run()
+
