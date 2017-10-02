ffccf23b62d03892b66bfb8025aca988946038be
boonleng <boonleng@ou.edu>
2017-10-02 11:36:26 +0900

    It trains!

--- eg2.py
+++ eg2.py
@@ -14,16 +14,21 @@ quotes = data.get_old_data()
 
 sym = 'NVDA'
 data = quotes[:, :, sym]
-y_close = np.array(data.loc[:, 'Close'].tolist(), dtype = np.float32)
-y_open = np.array(data.loc[:, 'Close'].tolist(), dtype = np.float32)
-#y_true = y_close < y_open
+y_close = np.array([data.loc[:, 'Close'].tolist()], dtype = np.float32)
+y_open = np.array([data.loc[:, 'Open'].tolist()], dtype = np.float32)
+mask = y_close[0] > y_open[0]
+ups = np.zeros((len(data), 2), dtype=np.float32)
+ups[~mask, 0] = 1.0   # Row 0 for down
+ups[mask, 1] = 1.0    # Row 1 for up
+
+kernel_size = 26
 
 def nn(x):
     """
         Neural network for analyzing the time series
     """
     with tf.name_scope('reshape'):
-        x_rect = tf.reshape(x, [-1, 26, 1, 1])
+        x_rect = tf.reshape(x, [-1, kernel_size, 1, 1])
 
     with tf.name_scope('conv1'):
         W_conv1 = weight_variable([5, 1, 1, 16])
@@ -34,10 +39,10 @@ def nn(x):
         h_pool1 = tf.nn.max_pool(h_conv1, ksize = [1, 2, 1, 1], strides = [1, 2, 1, 1], padding = 'SAME')
 
     with tf.name_scope('fc'):
-        W_fc = weight_variable([13 * 16, 512])
+        W_fc = weight_variable([int(kernel_size / 2) * 16, 512])
         b_fc = bias_variable([512])
 
-        h_pool1_flat = tf.reshape(h_pool1, [-1, 13 * 16])
+        h_pool1_flat = tf.reshape(h_pool1, [-1, int(kernel_size  / 2) * 16])
         h_fc = tf.nn.relu(tf.matmul(h_pool1_flat, W_fc) + b_fc)
 
     with tf.name_scope('droptout'):
@@ -64,12 +69,12 @@ def bias_variable(shape):
 
 print('Setting up \033[38;5;214mTensorflow\033[0m ...')
 
-x = tf.placeholder(tf.float32, [None, 26])
+x = tf.placeholder(tf.float32, [None, kernel_size])
 y_true = tf.placeholder(tf.float32, [None, 2])
-y_pred, keep_prob = nn(x)
+y_conv, keep_prob = nn(x)
 
 with tf.name_scope('loss'):
-	cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels = y_true, logits = y_pred)
+	cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels = y_true, logits = y_conv)
 
 cross_entropy = tf.reduce_mean(cross_entropy)
 
@@ -77,7 +82,7 @@ with tf.name_scope('adam_optimizer'):
 	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
 with tf.name_scope('accuracy'):
-    correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_true, 1))
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_true, 1))
     correct_prediction = tf.cast(correct_prediction, tf.float32)
     accuracy = tf.reduce_mean(correct_prediction)
 
@@ -89,12 +94,12 @@ train_writer.add_graph(tf.get_default_graph())
 
 N = 5
 
-#with tf.Session() as sess:
-#    sess.run(tf.global_variables_initializer())
-#    for i in range(200):
-#        batch = yy[i : i * 26]
-#        train_accuracy = accuracy.eval(feed_dict={
-#                                       x: batch[0], y_: batch[1], keep_prob: 1.0})
-#        print('step %d, training accuracy %g' % (i, train_accuracy))
-#
-#
+with tf.Session() as sess:
+   sess.run(tf.global_variables_initializer())
+   for i in range(len(data) - kernel_size):
+       train_accuracy = accuracy.eval(feed_dict={
+                                      x: y_close[:, i : i + kernel_size],
+                                      y_true: ups[i : i + kernel_size, :],
+                                      keep_prob: 0.99
+                                      })
+       print('step %d, training accuracy %.3f' % (i, train_accuracy))
