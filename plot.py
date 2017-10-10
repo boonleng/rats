dff7c9755a9a5bc3261540db310cace3f437a7d3
boonleng <boonleng@ou.edu>
2017-10-10 03:48:04 +0200

    Added command line option to open in Preview

--- plot.py
+++ plot.py
@@ -7,7 +7,9 @@ import mystyle
 def refresh():
     genfigs('^OLD')
 
-def genfigs(symbols, days = 90, sma_sizes = [10, 50, 100], folder = 'figs', color_scheme = 'default', image_format = 'png', verbose = 0):
+def genfigs(symbols, days = 90, sma_sizes = [10, 50, 100], folder = 'figs',
+            color_scheme = 'default', image_format = 'png', verbose = 0,
+            open_preview = False):
     # Get the latest data
     if symbols == '^OLD':
         stock = data.get_old_data()
@@ -34,6 +36,7 @@ def genfigs(symbols, days = 90, sma_sizes = [10, 50, 100], folder = 'figs', colo
         os.makedirs(folder)
 
     # Go through the symbols
+    command = 'open'
     for symbol in stock.minor_axis:
         if verbose > 1:
             print(stock[:, :, [(symbol)]])
@@ -49,6 +52,11 @@ def genfigs(symbols, days = 90, sma_sizes = [10, 50, 100], folder = 'figs', colo
             print('\033[38;5;196m{}\033[0m -> \033[38;5;206m{}\033[0m'.format(symbol.rjust(5), filename))
         # Save an image
         view.savefig(filename)
+        command = command + ' ' + filename
+
+    if open_preview:
+        os.system(command)
+
 
 #
 #  M A I N
@@ -58,6 +66,7 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--verbose', default = 1, action = 'count', help = 'increases verbosity level')
     parser.add_argument('-s', '--symbols', default = '^OLD', nargs = '+', help = 'specify symbols, e.g., -s NVDA TSLA AAPL')
     parser.add_argument('-c', '--color-scheme', default = 'default', help = 'specify color scheme to use.')
+    parser.add_argument('-o', '--open-preview', action = 'store_true', help = 'open Preview')
     parser.add_argument('-p', '--pdf', action = 'store_true', help = 'generate PDF.')
     parser.add_argument('-q', '--quiet', action = 'store_true', help = 'quiet mode.')
     args = parser.parse_args()
@@ -72,6 +81,8 @@ if __name__ == '__main__':
 
     # print('symbols = {}'.format(args.symbols))
     try:
-        genfigs(args.symbols, verbose = args.verbose, image_format = args.format, color_scheme = args.color_scheme)
+        genfigs(args.symbols,
+                verbose = args.verbose, image_format = args.format,
+                color_scheme = args.color_scheme, open_preview = args.open_preview)
     except KeyboardInterrupt:
         print('Exiting ...')
