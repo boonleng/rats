5111c6e449042342448b7730ebebf0cb34a6713f
boonleng <boonleng@ou.edu>
2017-10-18 18:48:25 -0500

    Variable naming for clarity

--- test-old-chart.py
+++ test-old-chart.py
@@ -8,8 +8,8 @@ stock = data.get_old_data(symbols = ['NVDA'])
 K = 90                       # Number of days to show
 sma_sizes = [10, 50, 200]     # SMA window sizes
 
-K = K + max(sma_sizes)
-s = stock[:, stock.axes[1][-K:], :]
+L = K + max(sma_sizes)
+s = stock[:, stock.axes[1][-L:], :]
 
 view = chart.showChart(s)
 
