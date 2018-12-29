b0ae47d3e7b7a1277c86cf0d47c5d0febcb4de78
boonleng <boonleng@ou.edu>
2018-12-29 17:30:08 -0600

    Notebook cleanup

--- mystyle.py
+++ mystyle.py
@@ -4,7 +4,7 @@ import matplotlib
 matplotlib.rcParams['font.family'] = 'serif'
 matplotlib.rcParams['font.serif'] = ['Arial']
 matplotlib.rcParams['font.sans-serif'] = ['System Font', 'Verdana', 'Arial']
-matplotlib.rcParams['figure.figsize'] = (7, 5)   # Change the size of plots
+matplotlib.rcParams['figure.figsize'] = (9, 5)   # Change the size of plots
 matplotlib.rcParams['figure.dpi'] = 108
 # matplotlib.rcParams['grid.alpha'] = 0.15
 # matplotlib.rcParams['text.color'] = gray
